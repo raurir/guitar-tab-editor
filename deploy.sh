fb20dce62babb14ed3e1bf1df075e96148d2511a
raurir <raurir@gmail.com>
2026-09-26 09:19:27 +1000

    nodryrun

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 yarn build
-aws s3 rm s3://raurir.com/apps/guitar-tab-editor/ --recursive --dryrun
-aws s3 sync ./dist s3://raurir.com/apps/guitar-tab-editor/ --dryrun
+aws s3 rm s3://raurir.com/apps/guitar-tab-editor/ --recursive
+aws s3 sync ./dist s3://raurir.com/apps/guitar-tab-editor/
 aws cloudfront create-invalidation --distribution-id E34F50VVZYJIRI --paths '/apps/guitar-tab-editor/*'
